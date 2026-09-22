664cbbecb9d9c1ec8700b1ca42b5b897e520153e
mvti11 <vlucenko669@gmail.com>
2026-09-22 17:53:06 +0300

    mvti
    Я ВЕРЮ ТЕБЕ ЭВЕЛИНА НАДЕЮСЬ ОНО НЕ СЛОМАЕТСЯ

---
+++ new_script.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print('прверка работы гида дай бог получится') # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
